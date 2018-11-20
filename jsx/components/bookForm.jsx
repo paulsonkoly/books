036d5d6387583eed28eafc24fe83304194f2fa88
Paul Sonkoly <sonkoly.pal@gmail.com>
2018-11-20 17:07:01 +0000

    Removed text from BookForm after submit

--- jsx/components/bookForm.jsx
+++ jsx/components/bookForm.jsx
@@ -3,19 +3,29 @@ import React from 'react';
 class BookForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title: '',
+      author: '',
+      isbn: '',
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    const data = new FormData(event.target);
-    const newBook = {
-      title: data.get('title'),
-      author: data.get('author'),
-      isbn: data.get('isbn'),
-    };
+    this.props.onSubmit(this.state);
+    this.setState({ title: '', author: '', isbn: '' })
+    window.focus();
+  }
 
-    this.props.onSubmit(newBook);
+  handleFormChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
@@ -24,19 +34,40 @@ class BookForm extends React.Component {
         <div className="form-group row">
           <label for="title" className="col-sm-2 col-form-label">Title</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" id="title" name="title" />
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleFormChange}
+            />
           </div>
         </div>
         <div className="form-group row">
           <label for="author" className="col-sm-2 col-form-label">Author</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" id="author" name="author" />
+            <input
+              type="text"
+              className="form-control"
+              id="author"
+              name="author"
+              value={this.state.author}
+              onChange={this.handleFormChange}
+            />
           </div>
         </div>
         <div className="form-group row">
           <label for="isbn" className="col-sm-2 col-form-label">ISBN</label>
           <div className="col-sm-10">
-            <input type="text" className="form-control" id="isbn" name="isbn" />
+            <input
+              type="text"
+              className="form-control"
+              id="isbn"
+              name="isbn"
+              value={this.state.isbn}
+              onChange={this.handleFormChange}
+            />
           </div>
         </div>
         <button type="submit" className="btn btn-primary mb-2">Create book</button>
